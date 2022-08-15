88286358a167aacab1e8d5b51d6f535c346a5dab
Naseer260 <naseer.it@gmail.com>
2022-08-15 14:37:39 +0530

    First commit

---
+++ eclipse-workspace/firstGame/src/temp/cardsNew.java
@@ -0,0 +1,52 @@
+package temp;
+
+import java.util.*;
+
+public class cardsNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i = 0;
+		String[] suit = {"Spade","Diamond","Hearts","Club"};
+		String s1 = "Spade";
+		String s2 = "Diamond";
+		int p1 = 4, p2 =3, p3=2, p4=1;
+		
+		for (i=0; i<4; i++)
+		{  
+			//System.out.println(suit[i]);
+			if(s1.equals(suit[i]) || s2.equals(suit[i]))
+			{ System.out.println("in match" + " " + suit[i]);
+					if(suit[i] == "Spade")
+					{ p1 = 4;
+					System.out.println(p1);
+					if(p1>=4 {
+						points1 = 1;
+						points2 = 0;
+					
+					
+						} else if (suit[i] == "Diamond")
+											{
+							p2=3;
+							System.out.println(p2);
+					}else if (suit[i] == "Hearts")
+					{
+						p3=2;
+						System.out.println(p3);
+					}
+						else {
+							p4=1;
+							System.out.println(p4);
+						}
+					System.out.println();
+					}
+			while (s1	
+		System.out.println();
+		}
+		
+		
+		
+						
+	}
+
+}
